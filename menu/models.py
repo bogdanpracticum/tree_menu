c76d73c3a92b6210b70c461e976f5a255f86d969
Bogdan Melikhov <Monpasie-prazdnik.ru@yandex.ru>
2023-05-02 14:14:33 +0300

    v1

---
+++ menu/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Menu(models.Model):
+    name = models.CharField('Название', max_length=100)
+    ulr = models.CharField('Ссылка', max_length=255)
+    position = models.PositiveIntegerField('Позиция', default=1)
+
+    def __str__(self):
+        return self.name
+
+
+class Meta:
+    ordering = ('position',)
+    verbose_name = 'Пункт меню'
+    verbose_name_plural = 'Пункты меню'
